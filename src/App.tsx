0dc52fe7caefe2914d6e0bf35a86ae9eed67634c
gyur1kim <qri1114@gmail.com>
2025-07-13 22:14:44 +0900

    chore: initial

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello World</h1>
+      <div className="text-red-500">안녕하세요</div>
+    </>
+  );
+}
+
+export default App;
